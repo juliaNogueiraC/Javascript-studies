d69f0a9b68fccb97ddf6001a48d38b55f826e518
juliaNogueiraC <69528739+juliaNogueiraC@users.noreply.github.com>
2023-05-19 20:59:24 +0000

    algoritmo String, If, e checked property

---
+++ if-statement.js
@@ -0,0 +1,12 @@
+let age = 12;
+
+if(age >= 18){
+  console.log("youre are an adult!");
+}
+else if(age < 0){
+  console.log("wtf")
+  
+}
+else{
+  console.log("you are a kid");
+}
---
+++ string.js
@@ -0,0 +1,12 @@
+// slice()
+
+
+
+// method chaining = calling onde method after another in one continous lide of code
+let userName = "peter";
+
+let letter= userName.charAt(0).toUpperCase();
+//ao inves de usar mais uma linha de codigo
+// letter = letter.toUpperCase();
+
+console.log(letter);
