4bbe7d30f9c21765d4c141f234586fc0c3f906f5
juliaNogueiraC <69528739+juliaNogueiraC@users.noreply.github.com>
2023-10-25 14:34:09 +0000

    Math

---
+++ math.js
@@ -0,0 +1,27 @@
+let x = 3.14; 
+let y = 3.14; 
+let z = 3.14; 
+
+x = math.round(x); // arrendodar
+x = math.ceil(x); // arrendoda p cima
+x = math.floor(x); // arredonda p baixo
+x = math.sqrt(x); // raiz quadrada
+x = math.pow(x, 2); // potencia
+x = math.log(x); // logaritmo
+x = math.log(x, 2); // logaritmo na base 2
+x = math.sin(x); // seno
+x = math.cos(x); // cosseno
+x = math.tan(x); // tangente
+x = math.abs(x); // modulo
+x = math.PI; // pi
+x = math.E; // euler
+x = math.round(x, 2); // arredonda 2 casas decimais
+x = math.floor(x, 2); // arredonda 2 casas decimais
+
+let maximum = Math.max(x, y, z); // maior valor
+let maximum = Math.min(x, y, z); // menor valor
+let maximum = Math.random(); // numero aleatorio
+let maximum = Math.random() * 10; // numero aleatorio entre 0 e 10
+let maximum = Math.random() * 10 + 1; // numero aleatorio entre 1 e 10
+
+console.log(x);
